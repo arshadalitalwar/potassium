e2a795c947affbb4dc59ea1551cd02de74ad968f
agarwalchirag140 <agarwalchirag140@gmail.com>
2020-12-31 23:08:19 +0530

    Deleted

--- index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("signin").addEventListener("click", login);
-document.getElementById("register").addEventListener("click", login);
-
-function login() {
-  document.getElementById("loginPop").style.display = "block";
-  document.getElementById("loginPop1").style.display = "block";
-}
